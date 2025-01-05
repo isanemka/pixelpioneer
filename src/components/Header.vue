279267867a4bd7063cf23b7f758d77762629347c
Anton Karlsson <isanemka@gmail.com>
2025-01-05 20:26:41 +0100

    Changed font and placement of main title

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,12 +4,16 @@
 <template>
   <header class="bg-darkbg text-white p-6">
     <nav class="container mx-auto flex justify-between items-center">
-      <h1 class="text-3xl font-bold font-freepixel">Pixel<br>Pioneer</h1>
-      <ul class="flex space-x-4 text-2xl font-freepixel">
-        <li><a href="#about" class="hover:text-indigo-400">Om mig</a></li>
-        <li><a href="#projects" class="hover:text-indigo-400">Projekt</a></li>
-        <li><a href="#skills" class="hover:text-indigo-400">Kompetenser</a></li>
-        <li><a href="#contact" class="hover:text-indigo-400">Kontakt</a></li>
+      <img
+        src="/public/rocket_with_stars.png"
+        alt="Rocket with stars"
+        class="w-16 h-16 mt-1"
+      />      
+      <ul class="flex space-x-4 text-2xl font-vt323">
+        <li><a href="#about" class="hover:font-vt323">Om mig</a></li>
+        <li><a href="#projects" class="hover:font-vt323">Projekt</a></li>
+        <li><a href="#skills" class="hover:font-vt323">Kompetenser</a></li>
+        <li><a href="#contact" class="hover:font-vt323">Kontakt</a></li>
       </ul>
     </nav>
   </header>
