4b8a17a75496c92689ada907e6f09ddea94fd29b
Anton Karlsson <isanemka@gmail.com>
2025-01-14 12:41:51 +0100

    Changed footer styling

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import './style.css'
-import './assets/tailwind.css'
-import App from './App.vue'
+import './assets/tailwind.css';
 
-createApp(App).mount('#app')
+// FontAwesome imports
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// Add the imported icons to the library
+library.add(faGithub, faLinkedin);
+
+const app = createApp(App);
+app.component('FontAwesomeIcon', FontAwesomeIcon);
+app.mount('#app');
